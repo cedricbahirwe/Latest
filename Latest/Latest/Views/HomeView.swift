e4525211877b3ff33bbecf6622799dd01a224e7c
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-16 09:13:46 +0200

    Add Screenshots

--- Latest/Latest/Views/HomeView.swift
+++ Latest/Latest/Views/HomeView.swift
@@ -30,69 +30,42 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
-struct NavBarView: View {
-    
-    @EnvironmentObject var data: AppManagerViewModel
-    let bgColor: Color = Color.mainColor
-    private let title: String = "Latest"
-    
-    var body: some View {
-        HStack {
-            Image(systemName: "person")
-                .imageScale(.large)
-                .onTapGesture {
-                    data.showProfileView.toggle()
-                }
-            Spacer()
-            Text("Latest")
-                .font(Font.title2.weight(.bold))
-            Spacer()
-            Image(systemName: "bookmark.circle")
-                .imageScale(.large)
-                .onTapGesture {
-                    data.showBookmarkView.toggle()
-                }
-        }
-        .foregroundColor(Color(.systemBackground))
-        .padding()
-        .frame(height: 48)
-        .background(bgColor.ignoresSafeArea(.all))
-    }
-}
-
-struct TopHeaderView: View {
-    let news: News
-    var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            Image("2")
-                .resizable()
-                .scaledToFit()
-            
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Security")
-                    .textCase(.uppercase)
-                    .font(Font.caption.weight(.semibold))
-                    .foregroundColor(Color.systemWhite.opacity(0.8))
-                Text("FBI launches operation to remove backdoors from hacked Hello Kigali servers")
-                    .font(Font.title.bold())
-                HStack(spacing: 5) {
-                    Text("By Cédric Bahirwe")
-                    Color.systemWhite
-                        .frame(width: 5, height: 5)
-                    Text(news.timeAgoDisplay)
+extension HomeView {
+    struct TopHeaderView: View {
+        let news: News
+        var body: some View {
+            ZStack(alignment: .bottomLeading) {
+                Image("2")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Security")
+                        .textCase(.uppercase)
+                        .font(Font.caption.weight(.semibold))
+                        .foregroundColor(Color.systemWhite.opacity(0.8))
+                    Text("FBI launches operation to remove backdoors from hacked Hello Kigali servers")
+                        .font(Font.title.bold())
+                    HStack(spacing: 5) {
+                        Text("By Cédric Bahirwe")
+                        Color.systemWhite
+                            .frame(width: 5, height: 5)
+                        Text(news.timeAgoDisplay)
+                        
+                    }
+                    .font(Font.caption2.weight(.light))
                     
                 }
-                .font(Font.caption2.weight(.light))
-                
+                .foregroundColor(.systemWhite)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Color.mainColor.opacity(0.8))
+                .padding(.trailing, 10)
             }
-            .foregroundColor(.systemWhite)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(Color.mainColor.opacity(0.8))
-            .padding(.trailing, 10)
+            .background(Color.mainColor.opacity(0.3))
+            .frame(minHeight: 150, alignment: .bottom)
+            
         }
-        .background(Color.mainColor.opacity(0.3))
-        .frame(minHeight: 150, alignment: .bottom)
-        
     }
+
 }
